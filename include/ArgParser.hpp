0cc3d5e49972ddf4b70779427441f813b0da9b2b
Henrique Jung <henrique.jung@senairs.org.br>
2026-02-10 09:15:24 -0300

    feat: argument list parser

--- include/ArgParser.hpp
+++ include/ArgParser.hpp
@@ -8,20 +8,40 @@
 #include <vector>
 class ArgParser {
 public:
-  ArgParser(const std::string &programName = "",
-            const std::string &description = "",
-            const TextProcessing &textProcessing = {});
+  ArgParser(const TextProcessing &textProcessing = {});
+
   void addPositionalArgument(const std::string &name,
                              const std::string &description = "");
   bool addOption(const ArgOption &option);
   bool addHelpOption();
   bool addVersionOption();
   void showHelp(std::ostream &out = std::cout) const;
+  void showVersion(std::ostream &out = std::cout) const;
   void parse(int argc, char *argv[]);
 
-  std::string programName;
-  std::string description;
-  TextProcessing textProcessing;
+  bool isSet(const std::string &option) const;
+  bool isSet(const ArgOption &option) const;
+
+  const std::vector<std::string> &values(const std::string &option) const;
+  const std::vector<std::string> &values(const ArgOption &option) const;
+  const std::string &value(const std::string &option) const;
+  const std::string &value(const ArgOption &option) const;
+
+  const std::vector<std::string> &positionalValues() const;
+  const std::unordered_map<size_t, std::vector<std::string>> &values() const;
+  const std::vector<ArgOption> &availableOptions() const;
+
+  template <class T> T typedValue(const ArgOption &option) const {
+    return typedValue<T>(option.arguments().front());
+  }
+
+  template <class T> T typedValue(const std::string &option) const;
+
+  std::string programName{};
+  std::string description{};
+  std::string version{"1.0"};
+  std::string uknownOptionMessage{"Unkown option(s): "};
+  TextProcessing textProcessing{};
 
 private:
   std::vector<std::string> makeArgsText(const std::vector<ArgOption> &args,
@@ -29,10 +49,16 @@ private:
   std::string makeSectionText(const std::vector<std::string> &argsText,
                               const size_t &longestLeft,
                               const std::vector<ArgOption> &args) const;
+  bool addParsedOption(const std::string &option);
+  void parseOption(std::vector<std::string>::const_iterator &it,
+                   std::vector<std::string>::const_iterator end);
+
   std::vector<ArgOption> _positionals{};
   bool _hasHelp{false};
-  std::string _hasVersion{""};
+  bool _hasVersion{false};
   std::vector<ArgOption> _options{};
   std::unordered_map<std::string, size_t> _optionPos{};
   std::unordered_map<size_t, std::vector<std::string>> _values{};
+  std::vector<std::string> _positionalValues{};
+  std::vector<std::string> _uknownValues{};
 };
--- src/ArgParser.cpp
+++ src/ArgParser.cpp
@@ -1,12 +1,10 @@
 #include "ArgParser.hpp"
 #include <algorithm>
+#include <exception>
 #include <sstream>
 
-ArgParser::ArgParser(const std::string &programName,
-                     const std::string &description,
-                     const TextProcessing &textProcessing)
-    : programName{programName}, description{description},
-      textProcessing{textProcessing} {}
+ArgParser::ArgParser(const TextProcessing &textProcessing)
+    : textProcessing{textProcessing} {}
 
 void ArgParser::addPositionalArgument(const std::string &name,
                                       const std::string &description) {
@@ -25,7 +23,7 @@ bool ArgParser::addOption(const ArgOption &option) { //
       return false;
     }
   }
-  const auto position = _optionPos.size();
+  const auto position = _options.size();
   _options.push_back(option);
   for (const auto &entry : entries) {
     _optionPos.insert({entry, position});
@@ -35,10 +33,12 @@ bool ArgParser::addOption(const ArgOption &option) { //
 }
 
 bool ArgParser::addHelpOption() {
+  _hasHelp = true;
   return addOption({{"-h", "--help"}, "Show help."});
 }
 
 bool ArgParser::addVersionOption() {
+  _hasVersion = true;
   return addOption({{"-v", "--version"}, "Show version."});
 }
 
@@ -74,10 +74,103 @@ void ArgParser::showHelp(std::ostream &out) const {
   }
 }
 
+void ArgParser::showVersion(std::ostream &out) const {
+  out << programName << " " << version << "\n";
+}
+
 void ArgParser::parse(int argc, char *argv[]) {
-  for (auto i = 0; i < argc; i++) {
-    const auto argument = argv[i];
+  _values.clear();
+  _positionalValues.clear();
+  _uknownValues.clear();
+
+  std::vector<std::string> arguments(argv, argv + argc);
+  auto it = arguments.cbegin();
+
+  if (programName.empty()) programName = *it;
+  it++;
+
+  for (; it != arguments.cend(); it++) {
+    const auto &argument = *it;
+    if (argument.compare("--") == 0) {
+      // TODO: store remaining arguments as unparsed
+    } else if (argument.starts_with('-')) {
+      parseOption(it, arguments.cend());
+    } else {
+      _positionalValues.push_back(argument);
+    }
   }
+
+  if (_hasHelp && isSet("-h")) {
+    showHelp();
+    exit(0);
+  }
+
+  if (_hasVersion && isSet("-v")) {
+    showVersion();
+    exit(0);
+  }
+
+  if (!_uknownValues.empty()) {
+    std::cerr << uknownOptionMessage << _uknownValues << std::endl;
+    showHelp();
+    exit(1);
+  }
+}
+
+bool ArgParser::isSet(const std::string &option) const { //
+  const auto position = _optionPos.find(option);
+  if (position == _optionPos.cend()) {
+    return false;
+  }
+  return _values.contains(position->second);
+}
+
+bool ArgParser::isSet(const ArgOption &option) const {
+  for (const auto &arg : option.arguments()) {
+    if (isSet(arg)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const std::vector<std::string> &
+ArgParser::values(const std::string &option) const {
+  const auto position = _optionPos.find(option);
+  if (position == _optionPos.cend()) {
+    throw std::invalid_argument("Uknown argument: " + option);
+  }
+  if (!_values.contains(position->second)) {
+    const auto &argOption = _options[position->second];
+    return argOption.defaultValues();
+  }
+  return _values.at(position->second);
+}
+
+const std::vector<std::string> &
+ArgParser::values(const ArgOption &option) const {
+  return values(option.arguments().front());
+}
+
+const std::string &ArgParser::value(const std::string &option) const {
+  return values(option).front();
+}
+
+const std::string &ArgParser::value(const ArgOption &option) const {
+  return values(option.arguments().front()).front();
+}
+
+const std::vector<std::string> &ArgParser::positionalValues() const {
+  return _positionalValues;
+}
+
+const std::unordered_map<size_t, std::vector<std::string>> &
+ArgParser::values() const {
+  return _values;
+}
+
+const std::vector<ArgOption> &ArgParser::availableOptions() const {
+  return _options;
 }
 
 std::vector<std::string>
@@ -103,4 +196,36 @@ ArgParser::makeSectionText(const std::vector<std::string> &argsText,
     it++;
   }
   return text;
+}
+
+bool ArgParser::addParsedOption(const std::string &option) { //
+  auto it = _optionPos.find(option);
+  if (it == _optionPos.end()) {
+    _uknownValues.push_back(option);
+    return false;
+  }
+  const auto &position = it->second;
+  _values.insert({position, {}});
+  return true;
+}
+
+void ArgParser::parseOption(std::vector<std::string>::const_iterator &it,
+                            std::vector<std::string>::const_iterator end) {
+  if (!addParsedOption(*it)) return; // TODO: unkown argument passed
+
+  const auto &position = _optionPos[*it];
+  const auto &option = _options[position];
+  const auto hasValue = !option.valueName().empty();
+  if (hasValue && it + 1 != end) {
+    it++;
+    _values[position].push_back(*it);
+  } else {
+    // TODO: value not passed with option that requires it
+  }
+}
+template <> int ArgParser::typedValue(const std::string &option) const {
+  return std::stoi(value(option));
+}
+template <> std::string ArgParser::typedValue(const std::string &option) const {
+  return value(option);
 }
